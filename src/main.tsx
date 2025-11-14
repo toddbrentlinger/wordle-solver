2b76e883bfe8f4a50f8e907179123f0af9e9e8e0
toddbrentlinger <toddbrentlinger@gmail.com>
2025-11-14 14:44:47 -0800

    Finish initial setup of project
    Installs additional packages to use SCSS styling, FontAwesome icons,
    and push builds to gh-pages branch on the github repository.
    Adds command line scripts to predeploy and deploy build to gh-pages
    branch on the github repository.
    Adds a homepage property to package.json file.
    Changes 'base' property inside vite.config.ts file to repository name.
    Imports FontAwesome into main.tsx file to be used on project.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@fortawesome/fontawesome-free/js/fontawesome.min.js'
+import '@fortawesome/fontawesome-free/js/solid.min.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
