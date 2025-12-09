a72e504732de0b8e2b43c0c5f50750078e145158
toddbrentlinger <toddbrentlinger@gmail.com>
2025-12-08 19:01:34 -0800

    Add form component to set new solution word
    Adds form component to change solution word and handle submit form event.

---
+++ src/components/SolutionWordForm.tsx
@@ -0,0 +1,46 @@
+import type { ChangeEvent, FormEvent } from "react";
+
+interface SolutionWordFormProps {
+    formSubmitHandler: (solutionWord: string) => void;
+    solutionWord: string;
+    setSolutionWord: (newSolutionWord: string) => void;
+}
+
+/**
+ * Form component to change solution word and handle submit event.
+ * 
+ * @param {SolutionWordFormProps} props 
+ * @returns {JSX.Element}
+ */
+function SolutionWordForm({ formSubmitHandler, solutionWord, setSolutionWord }: SolutionWordFormProps) {
+    /**
+     * Handles submit event for solution word form.
+     * @param {FormEvent<HTMLFormElement>} e 
+     */
+    const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        formSubmitHandler(solutionWord);
+    };
+
+    /**
+     * Handles change event for solution word form.
+     * @param {ChangeEvent<HTMLInputElement>} e 
+     */
+    const handleSolutionWordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSolutionWord(e.target.value);
+    };
+    
+    return (
+        <form onSubmit={ handleFormSubmit }>
+            <input
+                name="solution-word"
+                value={ solutionWord }
+                onChange={ handleSolutionWordChange }
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default SolutionWordForm;
